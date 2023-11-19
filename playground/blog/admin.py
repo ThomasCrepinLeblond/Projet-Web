8d4cc059e39eb9c4c37396d8f4435a156682bc52
ThomasCrepinLeblond <150866472+ThomasCrepinLeblond@users.noreply.github.com>
2023-11-20 00:02:31 +0100

    Delete playground directory

--- playground/blog/admin.py
+++
@@ -1,6 +0,0 @@
-
-from django.contrib import admin
-from .models import Membres, Equipement
-
-admin.site.register(Membres)
-admin.site.register(Equipement)
--- playground/blog/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
- 
-from .models import Membres
- 
-class MoveForm(forms.ModelForm):
- 
-    class Meta:
-        model = Membres
-        fields = ('lieu',)
